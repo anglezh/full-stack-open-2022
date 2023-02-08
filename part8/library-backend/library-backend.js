94301f2cc20268fa09210bbb9e9707241b3fa950
anglezh <inbzhanglei@163.com>
2023-02-08 22:27:15 +0800

    登录与更新缓存

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -190,7 +190,9 @@ const resolvers = {
       const user = await User.findOne({ username: args.username })
 
       if (!user || args.password !== 'secret') {
-        throw new UserInputError("wrong credentials")
+        throw new GraphQLError('wrong credentials', {
+          extensions: { code: 'BAD_USER_INPUT' }
+        })
       }
 
       const userForToken = {
